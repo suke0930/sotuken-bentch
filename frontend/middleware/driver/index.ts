d3ea236647ffa80ff19f895a32a80afe339c86f1
suke0930 <akitochuanshang@gmail.com>
2025-09-15 16:56:14 +0900

    tsconfigの整理

--- frontend/middleware/driver/index.ts
+++ frontend/middleware/driver/index.ts
@@ -1,4 +1,27 @@
-async function main() {
+import express from 'express';
+import path from 'path';
+/**
+ * メイン関数
+ * エントリポイントとして考えておｋ
+ * @param port サーバのポート
+ */
+async function main(port: number) {
+/**
+ * expressを使いwebサーバーを立てる
+ *  */;
+    const app = express();
 
+    app.get('/', (req, res) => {//ランディングページの実装
+        res.sendFile(path.join(__dirname, 'web', 'index.html'));
+    });
+    //
+
+
+    /**
+     * 実際のサーバー構築
+     */
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
 }
-main();
+main(12800);
