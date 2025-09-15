cc3c2fa09b5d54eaf7c10ea0f4bc994a01af01c3
suke0930 <akitochuanshang@gmail.com>
2025-09-15 16:57:26 +0900

    ランディングページ発行機能の整備

--- frontend/middleware/driver/index.ts
+++ frontend/middleware/driver/index.ts
@@ -11,14 +11,14 @@ async function main(port: number) {
  *  */;
     const app = express();
 
+    //サーバーの定義
     app.get('/', (req, res) => {//ランディングページの実装
         res.sendFile(path.join(__dirname, 'web', 'index.html'));
     });
-    //
 
 
     /**
-     * 実際のサーバー構築
+     * サーバー起動
      */
     app.listen(port, () => {
         console.log(`Server listening on port ${port}`);
